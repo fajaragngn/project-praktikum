5212489d96e0adffe90cecb9d42258d8ef7a6fe9
fajaragngn <fajaragungnugroho29@yahoo.com>
2020-11-02 15:38:00 +0700

    commit 1

---
+++ README.md
@@ -0,0 +1,6 @@
+## Pertemuan 5 - Latihan
+Buatlah program untuk menginputkan biodata terdiri dari nama lengkap, nama panggilan, NPM, tempat lahir, tanggal lahir, telepon, alamat.
+![soal 5](assets/img/pertemuan-5/1.png)
+
+berikut source code nya:
+![sc 5](assets/tugas-5.py)
---
+++ assets/tugas-5.py
@@ -0,0 +1,12 @@
+
+namalengkap = input("Please enter your full name : ") 
+namapanggilan = input("Please enter nickname : ")  
+npm = int(input("Please enter your NPM : "))
+tempatlahir = input("Please enter place of birth : ") 
+umur = int(input("Please enter year of birth : "))
+alamat = input("Please enter your address : ") 
+telepon = int(input("Please enter your phone number : "))
+
+print("\n\n Assalamu'alaikum. ")
+print("\n Let me introduce my self, my name is", namalengkap , "but you can call me", namapanggilan , "my NPM", npm , "I was born in", tempatlahir , "and iam", umur , "years old, I am very glad if you want to invite my house in", alamat , "So, don't forget to call me before with the number", telepon , "\n\n Thank you ")
+
